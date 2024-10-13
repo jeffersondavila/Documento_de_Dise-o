c9f19dd07ad8d5595c6df1e90b8dcdff9e0f4070
jeffersondavila <jeffersondavila16@gmail.com>
2024-10-12 19:55:27 -0600

    [ADD] Se mejoran las validaciones para las peticiones en swagger

--- Personal_Blog/BlogPersonal/BlogPersonal/Program.cs
+++ Personal_Blog/BlogPersonal/BlogPersonal/Program.cs
@@ -2,6 +2,7 @@ using BlogPersonal.Models;
 using BlogPersonal.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -16,7 +17,36 @@ builder.Services.AddEndpointsApiExplorer();
 // Configurar Swagger solo en desarrollo
 if (builder.Environment.IsDevelopment())
 {
-	builder.Services.AddSwaggerGen();
+	builder.Services.AddSwaggerGen(c =>
+	{
+		c.SwaggerDoc("v1", new OpenApiInfo { Title = "API de Blog Personal", Version = "v1" });
+
+		// Definir el esquema de seguridad para Swagger que permita el uso de JWT
+		c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+		{
+			Name = "Authorization",
+			Type = SecuritySchemeType.Http,
+			Scheme = "Bearer",
+			BearerFormat = "JWT",
+			In = ParameterLocation.Header,
+			Description = "Introduce el token JWT de esta manera: Bearer {tu token}"
+		});
+
+		c.AddSecurityRequirement(new OpenApiSecurityRequirement
+	{
+		{
+			new OpenApiSecurityScheme
+			{
+				Reference = new OpenApiReference
+				{
+					Type = ReferenceType.SecurityScheme,
+					Id = "Bearer"
+				}
+			},
+			Array.Empty<string>()
+		}
+	});
+	});
 }
 
 // Configurar la autenticación JWT
@@ -76,7 +106,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
 	app.UseSwagger();
-	app.UseSwaggerUI();
+	app.UseSwaggerUI(c =>
+	{
+		c.SwaggerEndpoint("/swagger/v1/swagger.json", "API de Blog Personal v1");
+		c.RoutePrefix = "swagger"; // Cambia el prefijo aquí si quieres acceder desde /swagger
+	});
 	app.UseDeveloperExceptionPage(); // Mostrar página de excepción detallada en desarrollo
 }
 else
