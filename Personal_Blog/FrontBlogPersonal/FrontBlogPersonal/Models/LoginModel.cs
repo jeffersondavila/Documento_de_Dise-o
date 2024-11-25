03ea4ef8a5121503acb55b9d498c41171a2ceee8
jeffersondavila <jeffersondavila16@gmail.com>
2024-11-24 18:32:57 -0600

    [ADD] Login V1

---
+++ Personal_Blog/FrontBlogPersonal/FrontBlogPersonal/Models/LoginModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FrontBlogPersonal.Models
+{
+	public class LoginModel
+	{
+		//[Required(ErrorMessage = "El correo es obligatorio.")]
+		//[EmailAddress(ErrorMessage = "El correo no tien un formato válido.")]
+		public string? Correo { get; set; }
+
+		//[Required(ErrorMessage = "El correo es obligatorio.")]
+		//[EmailAddress(ErrorMessage = "El correo no tien un formato válido.")]
+		public string? Password { get; set; }
+	}
+}
